431523f2b04a35ad3c1ce12c009e42180cd1886b
Francisco Junior <franciscojn49@gmail.com>
2020-06-02 12:07:03 -0300

    feat(project): created pages Home, Product and created routes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Home() {
+  return <h1>home</h1>;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Product from '../pages/Product';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/products" component={Product} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
